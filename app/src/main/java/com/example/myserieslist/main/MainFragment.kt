9de1830487385e1afca5ab15b6569d47e3031b08
Bruno Neves <bruno.neves@exaud.com>
2022-04-07 15:00:31 +0100

    MSL-1 - Added a click handler to play buttons into mainFragment

--- app/src/main/java/com/example/myserieslist/main/MainFragment.kt
+++ app/src/main/java/com/example/myserieslist/main/MainFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.myserieslist.R
+import com.example.myserieslist.databinding.FragmentDetailsBinding
+import com.example.myserieslist.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -15,7 +19,17 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(
+            inflater, R.layout.fragment_main, container,false)
+
+        binding.navButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_mainFragment_to_searchFragment)
+        }
+
+        binding.detailsButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_mainFragment_to_detailsFragment)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return binding.root
     }
 }
