c728df72d8ca6c770a6ca41ea524f20cf55aa70b
Bruno Neves <bruno.neves@exaud.com>
2022-04-26 12:59:03 +0100

    MSL-1 - Created Database and implement data class SeriesList Table

---
+++ app/src/main/java/com/example/myserieslist/data/model/Serie.kt
@@ -0,0 +1,14 @@
+package com.example.myserieslist.data.model
+
+import kotlinx.android.parcel.Parcelize
+
+//model of de database elements
+@Parcelize
+data class Serie(
+    val id: String,
+    val name: String,
+    val overview: String?,
+    val poster_path: String,
+    val first_air_date: String,
+    val vote_average: String,
+)
---
+++ app/src/main/java/com/example/myserieslist/data/remote/SeriesList.kt
@@ -0,0 +1,24 @@
+package com.example.myserieslist.data.remote
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.io.Serializable
+
+@Entity(tableName = "series_list")
+@Parcelize
+data class SeriesList(
+    val id_Serie: String,
+    val name: String,
+    val overview: String?,
+    val poster_path: String,
+    val first_air_date: String,
+    val vote_average: String,
+) : Serializable, Parcelable {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+    val baseUrl
+        get() = ""
+}
+
