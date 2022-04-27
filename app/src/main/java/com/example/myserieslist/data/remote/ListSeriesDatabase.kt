799b5b34662de74d7b6aa0a0758f0b9f6e2d9606
Bruno Neves <bruno.neves@exaud.com>
2022-04-27 12:02:54 +0100

    MSL-1 - Inserted and created queries

--- app/src/main/java/com/example/myserieslist/data/remote/ListSeriesDatabase.kt
+++ app/src/main/java/com/example/myserieslist/data/remote/ListSeriesDatabase.kt
@@ -3,9 +3,8 @@ package com.example.myserieslist.data.remote
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [SeriesList::class],
-    version = 1, exportSchema = false)
-
+@Database(entities = [Series::class], version = 1, exportSchema = false)
 abstract class ListSeriesDatabase : RoomDatabase(){
+
     abstract fun getSerieListDao() : SeriesListDao
 }
--- app/src/main/java/com/example/myserieslist/data/remote/SeriesListDao.kt
+++ app/src/main/java/com/example/myserieslist/data/remote/SeriesListDao.kt
@@ -3,19 +3,21 @@ package com.example.myserieslist.data.remote
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface SeriesListDao {
-    @Insert
-    suspend fun addToList(seriesList: SeriesList)
 
-    @Query("SELECT * FROM SERIES_LIST")
-    suspend fun getSeriesList(): LiveData<List<SeriesList>>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addToList(seriesList: Series)
 
-    @Query("SELECT count(*) FROM series_list WHERE series_list.id_Serie = :id ")
+    @Query("SELECT * FROM series")
+    suspend fun getSeriesList(): LiveData<List<Series>>
+
+    @Query("SELECT count(*) FROM series WHERE series.id_Serie = :id ")
     suspend fun checkSerie(id: String) : Int
 
-    @Query("DELETE FROM series_list WHERE series_list.id_Serie = :id")
+    @Query("DELETE FROM series WHERE series.id_Serie = :id")
     suspend fun removeFromList(id: String) : Int
 }
