de04a1932feacd7898a25dd519eee33379448573
Bruno Neves <bruno.neves@exaud.com>
2022-04-28 16:39:20 +0100

    MSL-1 - Created function to get api

--- app/src/main/java/com/example/myserieslist/api/SeriesApi.kt
+++ app/src/main/java/com/example/myserieslist/api/SeriesApi.kt
@@ -1,5 +1,9 @@
 package com.example.myserieslist.api
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface SeriesApi {
 
     companion object{
@@ -7,4 +11,10 @@ interface SeriesApi {
         const val BASE_URL = "https://api.themoviedb.org/3/"
         const val API_KEY = "87131f4b6436973f292f6ddfeb7e3a87"
     }
+
+    @GET("tv/popular")
+    fun getPopularSeries(
+        @Query("api_key") apiKey: String = API_KEY
+    ) : Call<SeriesResponse>
+
 }
