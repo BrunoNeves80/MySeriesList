0d3aa83771d291af3f284f2093ff349c5ab4fe69
Bruno Neves <bruno.neves@exaud.com>
2022-04-28 16:46:11 +0100

    MSL-1 - Created addToList, checkSerie and removeFromList functions

--- app/src/main/java/com/example/myserieslist/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/myserieslist/ui/details/DetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.myserieslist.R
 
 /**
--- app/src/main/java/com/example/myserieslist/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/myserieslist/ui/list/ListViewModel.kt
@@ -2,11 +2,16 @@ package com.example.myserieslist.ui.list
 
 import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import android.widget.Toast
+import androidx.lifecycle.*
+import androidx.room.Room
+import com.example.myserieslist.data.model.Serie
+import com.example.myserieslist.data.remote.Series
 import com.example.myserieslist.data.remote.getDatabase
 import com.example.myserieslist.repository.ListSerieRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ListViewModel(app: Application) : AndroidViewModel(app) {
 
@@ -28,4 +33,35 @@ class ListViewModel(app: Application) : AndroidViewModel(app) {
             throw IllegalArgumentException("Unable to construct viewmodel")
         }
     }
+
+    fun addToList() {
+        CoroutineScope(Dispatchers.IO).launch {
+            seriesRepository.addToList(
+                Series(
+                    "1a",
+                    "chico",
+                    "milk",
+                    "pacote",
+                    "do ar",
+                    "0"
+                )
+            )
+        }
+    }
+
+    fun checkSerie() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val serie = seriesRepository.checkSerie("4a")
+            Log.i("qqq", serie.name)
+        }
+    }
+
+    fun removeFromList() {
+        CoroutineScope(Dispatchers.IO).launch {
+            seriesRepository.removeFromList("3a")
+        }
+    }
+
+    //fun getListSerie() = seriesRepository.getListSerie(id)
+
 }
