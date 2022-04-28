47837cad1fe0e0cd5e2fef2f5376944ff8815b80
Bruno Neves <bruno.neves@exaud.com>
2022-04-28 16:42:07 +0100

    MSL-1 - Changed object

--- app/src/main/java/com/example/myserieslist/data/remote/SeriesListDao.kt
+++ app/src/main/java/com/example/myserieslist/data/remote/SeriesListDao.kt
@@ -15,8 +15,8 @@ interface SeriesListDao {
     @Query("SELECT * FROM series ORDER BY first_air_date DESC")
     fun getSeriesList(): LiveData<List<Series>>
 
-    @Query("SELECT count(*) FROM series WHERE series.id_Serie = :id ")
-    suspend fun checkSerie(id: String) : Int
+    @Query("SELECT * FROM series WHERE series.id_Serie = :id ")
+    suspend fun checkSerie(id: String) : Series
 
     @Query("DELETE FROM series WHERE series.id_Serie = :id")
     suspend fun removeFromList(id: String) : Int
