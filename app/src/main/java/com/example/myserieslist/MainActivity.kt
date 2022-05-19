13b5bc96fcdd29f5ff6d019aa154f5ca241b37f4
Bruno Neves <bruno.neves@exaud.com>
2022-05-19 10:20:30 +0100

    MSL-1 - Removed unused variable

--- app/src/main/java/com/example/myserieslist/MainActivity.kt
+++ app/src/main/java/com/example/myserieslist/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.myserieslist
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.myserieslist.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
             this,
             R.layout.activity_main
         )
-        val navController = this.findNavController(R.id.myNavHostFragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
