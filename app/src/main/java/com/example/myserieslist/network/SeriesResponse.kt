262cfd11248563746f8edfdfdfe961d27edcf8c1
Bruno Neves <bruno.neves@exaud.com>
2022-05-19 11:06:54 +0100

    MSL-1 - Added extra Line EOF and formated code

--- app/src/main/java/com/example/myserieslist/network/SeriesResponse.kt
+++ app/src/main/java/com/example/myserieslist/network/SeriesResponse.kt
@@ -1,9 +1,7 @@
 package com.example.myserieslist.network
 
 import com.example.myserieslist.data.model.Serie
-import com.example.myserieslist.data.remote.getDatabase
 
-
-data class SeriesResponse (
+data class SeriesResponse(
     val results: List<Serie>
 )
