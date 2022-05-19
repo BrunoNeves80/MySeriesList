1823f714abf2d8e02072ed837c64358c5efed676
Bruno Neves <bruno.neves@exaud.com>
2022-05-19 16:22:56 +0100

    MSL-1 - Updated files with new name

--- app/src/main/java/pt/brunoneves/myserieslist/repository/ListSerieRepository.kt
+++ app/src/main/java/pt/brunoneves/myserieslist/repository/ListSerieRepository.kt
@@ -1,15 +1,15 @@
 package pt.brunoneves.myserieslist.repository
 
-import pt.brunoneves.myserieslist.data.remote.Series
-import pt.brunoneves.myserieslist.data.remote.SeriesDatabase
+import pt.brunoneves.myserieslist.data.local.SeriesDatabase
+import pt.brunoneves.myserieslist.data.model.Series
 
 class ListSerieRepository constructor(
     private val database: SeriesDatabase
 ) {
     suspend fun addToList(seriesList: Series) = database.seriesListDao.addToList(seriesList)
-    suspend fun getListSerie(seriesList: Series) = database.seriesListDao.getSeriesList()
+    suspend fun getListSerie(seriesList: Series) = database.seriesListDao.getSeries()
     suspend fun checkSerie(id: String) = database.seriesListDao.checkSerie(id)
     suspend fun removeFromList(id: String) {
-        database.seriesListDao.removeFromList(id)
+        database.seriesListDao.removeSeries(id)
     }
 }
