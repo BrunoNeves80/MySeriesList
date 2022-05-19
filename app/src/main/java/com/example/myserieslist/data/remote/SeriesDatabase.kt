1ba51a52f2d9d7531f0e4b2783665d062b6b6aa9
Bruno Neves <bruno.neves@exaud.com>
2022-05-19 11:02:29 +0100

    MSL-1 - Added new line EOF

--- app/src/main/java/com/example/myserieslist/data/remote/SeriesDatabase.kt
+++ app/src/main/java/com/example/myserieslist/data/remote/SeriesDatabase.kt
@@ -1,15 +1,14 @@
 package com.example.myserieslist.data.remote
 
 import android.content.Context
-import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Series::class], version = 1)
-abstract class SeriesDatabase : RoomDatabase(){
+abstract class SeriesDatabase : RoomDatabase() {
 
-    abstract val seriesListDao : SeriesListDao
+    abstract val seriesListDao: SeriesListDao
 
 }
 
@@ -18,8 +17,10 @@ private lateinit var INSTANCE: SeriesDatabase
 fun getDatabase(context: Context): SeriesDatabase {
     synchronized(SeriesDatabase::class.java) {
         if (!::INSTANCE.isInitialized) {
-            INSTANCE = Room.databaseBuilder(context.applicationContext,
-                SeriesDatabase::class.java, "series_db").build()
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                SeriesDatabase::class.java, "series_db"
+            ).build()
         }
     }
     return INSTANCE
