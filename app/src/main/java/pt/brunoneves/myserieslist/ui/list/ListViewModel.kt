07f1527640445502f2cc4d8510b0e01655d34b11
Bruno Neves <bruno.neves@exaud.com>
2022-06-02 16:36:58 +0100

    MSL-2 - Created variable to call the repository and commented some code

--- app/src/main/java/pt/brunoneves/myserieslist/ui/list/ListViewModel.kt
+++ app/src/main/java/pt/brunoneves/myserieslist/ui/list/ListViewModel.kt
@@ -1,18 +1,21 @@
 package pt.brunoneves.myserieslist.ui.list
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import pt.brunoneves.myserieslist.repository.ListSerieRepository
+import android.view.View
+import androidx.lifecycle.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import pt.brunoneves.myserieslist.data.local.SeriesListDao
 import pt.brunoneves.myserieslist.data.local.getDatabase
+import pt.brunoneves.myserieslist.data.model.Series
+import pt.brunoneves.myserieslist.databinding.FragmentListBinding
 import pt.brunoneves.myserieslist.network.SeriesNetwork
+import pt.brunoneves.myserieslist.repository.ListSerieRepository
 
 class ListViewModel(app: Application) : AndroidViewModel(app) {
 
+    private lateinit var binding: FragmentListBinding
     /**
      * The data source this ViewModel will fetch results from.
      */
@@ -24,6 +27,46 @@ class ListViewModel(app: Application) : AndroidViewModel(app) {
                 return ListViewModel(app) as T
             }
             throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+
+    init {
+
+    }
+
+    val series = seriesRepository.getSeriesList()
+
+   /* private val _properties = MutableLiveData<List<Series>>()
+
+    val properties : LiveData<List<Series>>
+        get() = _properties
+
+
+   *//* private fun getSeriesProperties(filter: SeriesNetwork) {
+        viewModelScope.launch {
+            try {
+                _properties.value = SeriesNetwork.serie_service.getPopularSeries()
+            } catch (e : Exception) {
+                _properties.value
+            }
+        }
+    }*//*
+*/
+   fun displayList() {
+
+       val list = listOf<Series>()
+       if (list.isEmpty()) {
+           binding.recyclerViewSerie.visibility = View.GONE
+           binding.emptyList.visibility = View.VISIBLE
+       } else {
+           binding.recyclerViewSerie.visibility = View.VISIBLE
+           binding.emptyList.visibility = View.GONE
+       }
+   }
+
+    fun getListSerie() {
+        CoroutineScope(Dispatchers.IO).launch {
+            SeriesNetwork.serie_service.getPopularSeries()
         }
     }
 
@@ -56,9 +99,4 @@ class ListViewModel(app: Application) : AndroidViewModel(app) {
         }
     }*/
 
-    fun getListSerie() {
-        CoroutineScope(Dispatchers.IO).launch {
-            SeriesNetwork.serie_service.getPopularSeries()
-        }
-    }
 }
