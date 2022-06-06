35eeeeaeb10bcee5abcf3563de43cb8ad08a5334
Bruno Neves <bruno.neves@exaud.com>
2022-06-06 15:37:03 +0100

    MSL-2 - Used Glide To show the image of Serie

--- app/src/main/java/pt/brunoneves/myserieslist/ui/list/ListSeriesAdapter.kt
+++ app/src/main/java/pt/brunoneves/myserieslist/ui/list/ListSeriesAdapter.kt
@@ -3,12 +3,16 @@ package pt.brunoneves.myserieslist.ui.list
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import pt.brunoneves.myserieslist.R
 import pt.brunoneves.myserieslist.data.model.Series
 
-class ListSeriesAdapter: RecyclerView.Adapter<ListSeriesAdapter.ListSerieViewHolder>() {
+
+class ListSeriesAdapter : RecyclerView.Adapter<ListSeriesAdapter.ListSerieViewHolder>() {
     var data = listOf<Series>()
         set(value) {
             field = value
@@ -25,11 +29,20 @@ class ListSeriesAdapter: RecyclerView.Adapter<ListSeriesAdapter.ListSerieViewHol
     override fun onBindViewHolder(holder: ListSerieViewHolder, position: Int) {
         val item = data[position]
         holder.serieList.text = item.name
+        Glide.with(holder.image)
+            .load("https://image.tmdb.org/t/p/w500" + item.poster_path)
+            .apply(options)
+            .into(holder.image)
     }
+    var options: RequestOptions = RequestOptions()
+        .centerCrop()
+        .placeholder(R.mipmap.ic_launcher_round)
+        .error(R.mipmap.ic_launcher_round)
 
     override fun getItemCount(): Int = data.size
 
     class ListSerieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val serieList: TextView = itemView.findViewById(R.id.serie_title)
+        val image: ImageView = itemView.findViewById(R.id.serie_poster)
     }
 }
