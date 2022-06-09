5f405a8424a489c0d561a3b6ff67413e79199941
Bruno Neves <bruno.neves@exaud.com>
2022-06-09 10:32:55 +0100

    MSL-3 - Created class Factory

--- app/src/main/java/pt/brunoneves/myserieslist/ui/search/SearchViewModel.kt
+++ app/src/main/java/pt/brunoneves/myserieslist/ui/search/SearchViewModel.kt
@@ -1,4 +1,17 @@
 package pt.brunoneves.myserieslist.ui.search
 
-class SearchViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class SearchViewModel (app: Application) : AndroidViewModel(app) {
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
+                return SearchViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
 }
