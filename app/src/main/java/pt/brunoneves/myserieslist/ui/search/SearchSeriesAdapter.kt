21a0d429e67d26e077fcc6e16a8aac83cc0c63f4
Bruno Neves <bruno.neves@exaud.com>
2022-06-13 14:51:03 +0100

    MSL-3 - Created onClick handler for the image

--- app/src/main/java/pt/brunoneves/myserieslist/ui/search/SearchSeriesAdapter.kt
+++ app/src/main/java/pt/brunoneves/myserieslist/ui/search/SearchSeriesAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -37,6 +38,10 @@ class SearchSeriesAdapter: RecyclerView.Adapter<SearchSeriesAdapter.SearchSerieV
             .load("https://image.tmdb.org/t/p/w300" + item.poster_path )
             .apply(options)
             .into(holder.image)
+
+        holder.image.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_searchFragment_to_detailsFragment)
+        }
     }
 
     override fun getItemCount(): Int = data.size
