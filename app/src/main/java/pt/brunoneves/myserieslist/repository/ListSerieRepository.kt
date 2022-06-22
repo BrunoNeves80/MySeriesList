869e62b73fa5b5a3b38ffd1d00595de6ad52d2e0
Bruno Neves <bruno.neves@exaud.com>
2022-06-22 12:26:37 +0100

    MSL-3 - Created new functions getPopularSeries() and getSeriesByName() in the ListSerieRepository class

--- app/src/main/java/pt/brunoneves/myserieslist/repository/ListSerieRepository.kt
+++ app/src/main/java/pt/brunoneves/myserieslist/repository/ListSerieRepository.kt
@@ -2,6 +2,7 @@ package pt.brunoneves.myserieslist.repository
 
 import pt.brunoneves.myserieslist.data.local.SeriesDatabase
 import pt.brunoneves.myserieslist.data.model.Series
+import pt.brunoneves.myserieslist.network.SeriesNetwork
 
 class ListSerieRepository constructor(
     private val database: SeriesDatabase
@@ -11,5 +12,13 @@ class ListSerieRepository constructor(
     suspend fun getSeries(id: String) = database.seriesListDao.getSeries(id)
     suspend fun deleteSeries(id: String) {
         database.seriesListDao.deleteSeries(id)
+    }
+
+    suspend fun getPopularSeries(): List<Series> {
+        return SeriesNetwork.serie_service.getPopularSeries().results
+    }
+
+    suspend fun getSeriesByName(name: String): List<Series> {
+        return SeriesNetwork.serie_service.getSeriesByName(name).results
     }
 }
