ee1a1f0330257e6de7c07cf0ebe5883d5b43aa4c
Bruno Neves <bruno.neves@exaud.com>
2022-06-23 10:41:00 +0100

    MSL-3 - Changed name of variable

--- app/src/main/java/pt/brunoneves/myserieslist/ui/search/SearchFragment.kt
+++ app/src/main/java/pt/brunoneves/myserieslist/ui/search/SearchFragment.kt
@@ -18,7 +18,7 @@ class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
 
-    private val viewModel: SearchViewModel by lazy {
+    private val searchViewModel: SearchViewModel by lazy {
         val activity = requireNotNull(this.activity) {
             "You can only access the viewModel after onActivityCreated()"
         }
