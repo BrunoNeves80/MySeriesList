f3ca19e3073707644c63db79c88e73db6f824846
Bruno Neves <bruno.neves@exaud.com>
2022-06-29 17:01:00 +0100

    MSL-4 - Added new variables

--- app/src/main/java/pt/brunoneves/myserieslist/network/SeriesDetailsResponse.kt
+++ app/src/main/java/pt/brunoneves/myserieslist/network/SeriesDetailsResponse.kt
@@ -7,4 +7,6 @@ data class SeriesDetailsResponse(
     val poster_path: String?,
     val first_air_date: String?,
     val vote_average: String,
+    val number_of_episodes: String,
+    val number_of_seasons: String
 )
