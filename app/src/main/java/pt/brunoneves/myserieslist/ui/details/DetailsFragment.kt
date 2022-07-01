1d3a7037a2801f36c29ccb1e532cb778cc07a284
Bruno Neves <bruno.neves@exaud.com>
2022-07-01 15:44:17 +0100

    MSL-4 - Created CoroutineScope  with new variables to show details into DetailsFragment and changed return statement to root

--- app/src/main/java/pt/brunoneves/myserieslist/ui/details/DetailsFragment.kt
+++ app/src/main/java/pt/brunoneves/myserieslist/ui/details/DetailsFragment.kt
@@ -1,23 +1,81 @@
 package pt.brunoneves.myserieslist.ui.details
 
-import android.os.Bundle
-import android.view.*
-import androidx.fragment.app.Fragment
-import pt.brunoneves.myserieslist.R
 
-/**
- * A simple [Fragment] subclass.
- */
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.fragment_details.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import pt.brunoneves.myserieslist.R
+import pt.brunoneves.myserieslist.databinding.FragmentDetailsBinding
+import pt.brunoneves.myserieslist.repository.ListSerieRepository
+import pt.brunoneves.myserieslist.ui.search.SearchSeriesAdapter
+import pt.brunoneves.myserieslist.ui.search.SearchViewModel
+
 class DetailsFragment : Fragment() {
+    private lateinit var detailsViewModel: DetailsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = DataBindingUtil.inflate<FragmentDetailsBinding>(
+            inflater,
+            R.layout.fragment_details, container, false
+        )
+
+        // Get a reference to the ViewModel associated with this fragment.
+        detailsViewModel = ViewModelProvider(this)
+            .get(DetailsViewModel::class.java)
 
         setHasOptionsMenu(true)
 
+        binding.lifecycleOwner = this
+
+        CoroutineScope(Dispatchers.IO).launch {
+            var id = arguments?.getInt("id", 0)
+            var series = detailsViewModel.getSeriesDetails(id!!)
+            Log.i("tag", "id $series")
+            var options = RequestOptions()
+                .centerCrop()
+                .placeholder(R.mipmap.ic_launcher_round)
+                .error(R.mipmap.ic_launcher_round)
+
+            requireActivity().runOnUiThread {
+                binding.tvSerieTitle.text = series.name
+                binding.tvDescription.text = series.overview
+                binding.tvReleaseDate.text = series.first_air_date
+                binding.tvVoteAverage.text = series.vote_average
+                binding.tvNumberEpisodes.text = series.number_of_episodes
+                binding.tvNumberSeasons.text = series.number_of_seasons
+                Glide.with(detail_serie_poster)
+                    .load("https://image.tmdb.org/t/p/w300" + series.poster_path)
+                    .apply(options)
+                    .into(detail_serie_poster)
+                binding.detailSeriePoster.visibility = View.VISIBLE
+                binding.tvSerieTitle.visibility = View.VISIBLE
+                binding.tvDescription.visibility = View.VISIBLE
+                binding.tvReleaseDate.visibility = View.VISIBLE
+                binding.tvVoteAverage.visibility = View.VISIBLE
+                binding.tvNumberEpisodes.visibility = View.VISIBLE
+                binding.tvNumberSeasons.visibility = View.VISIBLE
+                binding.progressBar.visibility = View.GONE
+                binding.NoResults.visibility = View.GONE
+
+            }
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        return binding.root
     }
 }
